85a192b1e00d8abc1bf3e6e17521a248a3929b72
mariozhou <chenmario32@gmail.com>
2020-06-01 21:36:18 -0600

    Guardar edad listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 /* import { ReactiveFormsModule } from "@angular/forms"; */
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from "@angular/forms";
 
 //Rutas
 import { app_routing } from "./app.routes";
@@ -35,7 +36,8 @@ import { HomeComponent } from './components/home/home.component';
     BrowserModule,
     AppRoutingModule,
     app_routing,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [InformacionService],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from "@angular/forms";
 import { NgModule } from '@angular/core';
-import { MatFormFieldModule   } from '@angular/material/form-field/form-field-module';
-import {  } from "@angular/material/select/select-module";
+import { MatFormFieldModule } from '@angular/material/form-field/form-field-module';
+import { } from "@angular/material/select/select-module";
+import { debounceTime } from "rxjs/operators";
 //MatInputModule, MatPaginatorModule,MatSortModule, MatTableModule
 interface Food {
   value: string;
@@ -17,29 +18,43 @@ interface Food {
 
 
 
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  form: FormGroup;
+
 
   /* form: FormGroup; */
- //combobox select
-/*   foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
-  ]; */
+  //combobox select
+  /*   foods: Food[] = [
+     {value: 'steak-0', viewValue: 'Steak'},
+     {value: 'pizza-1', viewValue: 'Pizza'},
+     {value: 'tacos-2', viewValue: 'Tacos'}
+   ]; */
 
-  constructor() { 
-  /*   this.buildForm(); */
+  constructor() {
+    this.buildForm();
   }
 
- /*   private buildForm() {
-    this.form = new FormGroup({
-      edad: new FormControl('', [Validators.required]),
- 
-    }); */
+  ngOnInit() {
+  }
 
-   /*  this.form.valueChanges
+
+  save(event: Event) {
+    event.preventDefault();
+    const value = this.form.value;
+    console.log(value);
+  }
+
+  private buildForm() {
+    this.form = new FormGroup({
+      tipotest: new FormControl('', [Validators.required])
+    });
+
+  /*   this.form.valueChanges
       .subscribe(value => {
         console.log(value);
       }); */
-  
+
+      
+    
+  }
 }
